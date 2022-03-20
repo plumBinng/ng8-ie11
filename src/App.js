56fef0671460d1265b2cd4fe6d45952f6148f5a8
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-03-20 20:00:50 +0000

    removed old test

--- src/App.js
+++ src/App.js
@@ -112,4 +112,7 @@ class App extends React.Component {
         }
       }
     }
-    this.setState({accounts: newAccou
+    this.setState({accounts: newAccounts})
+    this.accountsData()
+    return newAccounts
+ 
