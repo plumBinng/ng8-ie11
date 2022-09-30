d2247b6f043c6a21fc334eb8f58e961ae51b6a5b
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-09-30 05:36:19 +0000

    Changes ...

--- src/ArrowUp.js
+++ src/ArrowUp.js
@@ -8,4 +8,8 @@ const ArrowUp = props => (
     data-prefix="far"
     data-icon="arrow-alt-circle-up"
     role="img"
-    xmlns="http://www.w3.or
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+  >
+    <path
+      fill="#9
