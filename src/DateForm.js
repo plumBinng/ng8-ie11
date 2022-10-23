b3c6de60f1f8e59243eaef4d1d03dc5e241b2f1c
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-10-23 07:22:28 +0000

    Update documentation/p0x01/Software.md

--- src/DateForm.js
+++ src/DateForm.js
@@ -60,3 +60,69 @@ class DateForm extends React.Component {
     let day = event.target.value
     if (day.length < 2) {
       day = '0' + day
+    }
+    this.setState({
+      toDay: day
+    })
+  }
+
+  handleSubmit(event) {
+    let fromDate = `${this.state.fromYear}.${this.state.fromMonth}.${this.state.fromDay}`
+    let toDate = `${this.state.toYear}.${this.state.toMonth}.${this.state.toDay}`
+    let unixFinalDateFrom = new Date(fromDate).getTime()
+    let unixFinalDateTo = new Date(toDate).getTime()
+    this.props.handleSubmitTime(unixFinalDateFrom, unixFinalDateTo)
+    event.preventDefault()
+  }
+
+  render() {
+    let arr = new Array(31)
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = i + 1
+    }
+    return (
+      <Col sm={8}>
+        <div>
+          <Form onSubmit={this.handleSubmit}>
+            <Container>
+              <Row className="justify-content-md-center">
+                <Form.Label>Filter by Date Range:</Form.Label>
+              </Row>
+              <Row>
+                <Col className="dateDropdowns">
+                  <Form.Control
+                    as="select"
+                    name="year"
+                    onChange={this.handleYearChangeFrom}
+                    className="yearDropdown"
+                  >
+                    <option value="2019">2019</option>
+                    <option value="2018">2018</option>
+                  </Form.Control>
+                  <Form.Control
+                    as="select"
+                    name="month"
+                    onChange={this.handleMonthChangeFrom}
+                    className="monthDropdown"
+                  >
+                    <option value="01">Jan</option>
+                    <option value="02">Feb</option>
+                    <option value="03">Mar</option>
+                    <option value="04">Apr</option>
+                    <option value="05">May</option>
+                    <option value="06">Jun</option>
+                    <option value="07">Jul</option>
+                    <option value="08">Aug</option>
+                    <option value="09">Sep</option>
+                    <option value="10">Oct</option>
+                    <option value="11">Nov</option>
+                    <option value="12">Dec</option>
+                  </Form.Control>
+                  <Form.Control
+                    as="select"
+                    name="day"
+                    onChange={this.handleDateChangeFrom}
+                    className="dayDropdown"
+                  >
+                    {arr.map(x => (
+                      <option value={x} key={x}>
