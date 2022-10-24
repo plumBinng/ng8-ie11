d40d17054ca596cc2ccf4d195d8af3bd192a0868
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-10-24 17:37:55 +0000

    Added CONTRIBUTING.md file

--- src/FilterAccountForm.js
+++ src/FilterAccountForm.js
@@ -1,4 +1,8 @@
 import React from 'react'
 import {Col, Form} from 'react-bootstrap'
 
-const FilterAccountFor
+const FilterAccountForm = props => {
+  return (
+    <Col sm={4}>
+      <Form>
+        <Form.Lab
