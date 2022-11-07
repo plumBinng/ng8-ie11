4211dfdf8e89c3920c356ac2c6b0ce556197b238
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-11-07 23:10:39 +0000

    fixed a warning

--- src/LineChart.js
+++ src/LineChart.js
@@ -2,4 +2,6 @@ import React from 'react'
 import {Line} from 'react-chartjs-2'
 import {Container} from 'react-bootstrap'
 
-class LineChart extends R
+class LineChart extends React.Component {
+  render() {
+    let label = this.props.select
