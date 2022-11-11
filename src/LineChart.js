587da6daf86adb7d9013b9ebb2cc93d714cb7c9b
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-11-11 12:16:25 +0000

    Add Changes entry

--- src/LineChart.js
+++ src/LineChart.js
@@ -6,4 +6,6 @@ class LineChart extends React.Component {
   render() {
     let label = this.props.selectedAccount
     let labels = this.props.transactions.map(x => x.displayDate).reverse()
-    let balance = this.props.transactions.map(x =>
+    let balance = this.props.transactions.map(x => x.balance).reverse()
+    const data = {
+      labels: l
