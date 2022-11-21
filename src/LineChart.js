b5dfa19ef7a0ab36b84c314bc73cf556f1ce1177
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-11-21 21:12:03 +0000

    Import

--- src/LineChart.js
+++ src/LineChart.js
@@ -24,4 +24,5 @@ class LineChart extends React.Component {
           pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220
