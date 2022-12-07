1d23c0fad54e63bf6120ff8c7178bc332efd24e3
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-12-07 18:24:05 +0000

    New example

--- src/NoResults.js
+++ src/NoResults.js
@@ -1,4 +1,9 @@
 import React from 'react'
 const NoResults = () => (
   <p className="noResults">
-    Please try again, there were <strong>0</s
+    Please try again, there were <strong>0</strong> results for your search
+    criteria!
+  </p>
+)
+
+export default N
