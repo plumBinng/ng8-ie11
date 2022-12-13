f82c652f913b3492ab2efa4d36518ee0f26ce864
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-12-13 11:34:54 +0000

    2.8.5

--- src/SortByDropdown.js
+++ src/SortByDropdown.js
@@ -5,4 +5,6 @@ const SortByDropdown = props => (
   <Col sm={4}>
     <Form>
       <Form.Label>Sort By:</Form.Label>
-      <Form.Control as="select" name="sortBy" onChange={props.handleSortInp
+      <Form.Control as="select" name="sortBy" onChange={props.handleSortInput}>
+        <option value="dateDes">Date Descending</option>
+     
