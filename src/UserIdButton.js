89d0bc625974b62fa8e35d62e7102f20c6dc6428
plumBinng <91535981+plumBinng@users.noreply.github.com>
2022-12-20 12:39:54 +0000

    changed about

--- src/UserIdButton.js
+++ src/UserIdButton.js
@@ -2,4 +2,9 @@ import React from 'react'
 import {Button, Container, Col, Row} from 'react-bootstrap'
 
 class UserIdButton extends React.Component {
-  construct
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    let us
