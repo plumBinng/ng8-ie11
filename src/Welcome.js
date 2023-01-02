fee4e0d85aa8d6843ce59eaa02a118ab99521cad
plumBinng <91535981+plumBinng@users.noreply.github.com>
2023-01-02 13:28:37 +0000

    modified composer.json

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,2 +1,30 @@
 
 import React from 'react'
+import {Container, Row, Col, Jumbotron} from 'react-bootstrap'
+
+const Welcome = () => {
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col sm={9}>
+          <Jumbotron>
+            <Container>
+              <h1>
+                Welcome to{' '}
+                <span className="blueText">
+                  <strong>Grasshopper</strong>
+                </span>{' '}
+                Bank!
+              </h1>
+              <p className="pleaseSignIn">
+                Please sign in by selecting your <strong>user ID</strong> below:
+              </p>
+            </Container>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Welcome
