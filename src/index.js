c182265aca6f6d96eab79ddeb4ae7d4fda5ecaa6
plumBinng <91535981+plumBinng@users.noreply.github.com>
2023-01-04 20:21:41 +0000

    fixed changelog

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App 
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// If you want your app t
