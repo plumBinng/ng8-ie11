c98c841d8e4c448683977e9a52f4d2cc00d0b516
plumBinng <91535981+plumBinng@users.noreply.github.com>
2023-01-04 12:40:23 +0000

    update lbry-redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from '.
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+ReactDOM.render(<App 
