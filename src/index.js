b9e89fdca36ff1d9b6492c731e8070f0d4d33426
plumBinng <91535981+plumBinng@users.noreply.github.com>
2023-01-05 12:56:45 +0000

    Add changelog item

--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-// If you want your app t
+// If you want your app to work offline and load faster, you can change
+// unreg
