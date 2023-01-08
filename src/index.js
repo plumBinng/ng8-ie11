34e20db1e4075de0ea1f54d4bac289522fdec21f
plumBinng <91535981+plumBinng@users.noreply.github.com>
2023-01-08 07:28:46 +0000

    caption

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ import * as serviceWorker from './serviceWorker'
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some p
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://b
